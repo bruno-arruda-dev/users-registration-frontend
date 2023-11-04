71fb6a9093f6afa4a7ef62fb595ac55958961ddb
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2023-11-03 22:30:57 -0300

    Inicio da configuração do projeto.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
